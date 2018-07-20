202db585d1a0cb23a5e1ffdf7ba16c121407fb37
tomas9807 <tomas9807@protonmail.com>
2018-07-20 11:25:08 -0400

    >)

--- blog_project/blog_app/admin.py
+++ blog_project/blog_app/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','timestamp','updated']
+    list_display_links = ['updated']
+    list_filter = ['updated']
+    list_editable = ['title']
+    search_fields = ['title','content']
+
+admin.site.register(models.Post,PostAdmin)
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def post_create(request):
+    return HttpResponse('ma')
+
+def post_detail(request):
+    return HttpResponse('ma')
+
+def post_list(request):
+    return HttpResponse('ma')
+
+def post_update(request):
+    return HttpResponse('ma')
+    
+def post_delete(request):
+    return HttpResponse('ma')
